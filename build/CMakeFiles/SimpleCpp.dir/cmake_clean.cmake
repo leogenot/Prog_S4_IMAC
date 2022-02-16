cdde91b2a458b08afed800ccf4e595e4ec0cc88c
Léo Genot <45610291+leogenot@users.noreply.github.com>
2022-02-16 16:42:36 +0100

    jules template

---
+++ build/CMakeFiles/SimpleCpp.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../bin/Debug/SimpleCpp"
+  "../bin/Debug/SimpleCpp.pdb"
+  "CMakeFiles/SimpleCpp.dir/src/main.cpp.o"
+  "CMakeFiles/SimpleCpp.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleCpp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
