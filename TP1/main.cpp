1878f6f5e805bab0a7bd77da4c1a0346588a6ecf
Léo Genot <45610291+leogenot@users.noreply.github.com>
2022-02-23 14:36:33 +0100

    added p6 lib

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <random>
 #include "include/menu.h"
-
+#include <p6/p6.h>
 int main()
 {
-    show_menu();
+    //show_menu();
+    auto ctx = p6::Context{{1280, 720, "My p6 project"}}; // Create a context with a window
+    ctx.start();        
+
 }
