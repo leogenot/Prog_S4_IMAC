8770f44013f6545055b8964793bce287d973ff4e
Léo Genot <45610291+leogenot@users.noreply.github.com>
2022-02-24 11:09:51 +0100

    p6 setup

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -2,10 +2,17 @@
 #include <random>
 #include "include/menu.h"
 #include <p6/p6.h>
+
+
 int main()
 {
-    //show_menu();
-    auto ctx = p6::Context{{1280, 720, "My p6 project"}}; // Create a context with a window
-    ctx.start();        
-
+    // show_menu();
+    auto ctx   = p6::Context{{500, 500, "Nougs and "}};
+         ctx.background({0.5f, 0.3f, 0.8f}); 
+     ctx.update = [&]() {
+         ctx.background({0.5f, 0.3f, 0.8f}); // Clear the background with some color (Try to comment out this line to see what happens)
+        ctx.circle(p6::Center{ctx.mouse()}, // Draw a circle centered on the mouse cursor
+                   p6::Radius{0.3f});  
+         };
+     ctx.start();
 }
