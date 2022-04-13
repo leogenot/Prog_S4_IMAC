4dd2365a0aebfebe2ff08409e55a5400f2705e69
Léo Genot <leogenot@gmail.com>
2022-04-13 16:23:23 +0200

    added grid

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -3,16 +3,29 @@
 #include "include/menu.h"
 #include <p6/p6.h>
 
+void draw_board(int size, p6::Context& ctx)
+ {
+     for (int x = 0; x < size; ++x) {
+         for (int y = 0; y < size; ++y) {
+             ctx.square(p6::BottomLeftCorner{p6::map(glm::vec2{x, y},
+                                                     glm::vec2{0.f}, glm::vec2{static_cast <float> (size)},
+                                                     glm::vec2{-1.f}, glm::vec2{1.f})},
+                        p6::Radius{1.f / size});
+         }
+     }
+ }
 
 int main()
 {
     // show_menu();
-    auto ctx   = p6::Context{{500, 500, "Nougs and "}};
-         ctx.background({0.5f, 0.3f, 0.8f}); 
-     ctx.update = [&]() {
-         ctx.background({0.5f, 0.3f, 0.8f}); // Clear the background with some color (Try to comment out this line to see what happens)
-        ctx.circle(p6::Center{ctx.mouse()}, // Draw a circle centered on the mouse cursor
-                   p6::Radius{0.3f});  
-         };
-     ctx.start();
+    auto ctx = p6::Context{{500, 500, "Nougs and "}};
+    ctx.background({0.5f, 0.3f, 0.8f});
+    ctx.update = [&]()
+    {
+        ctx.background({0.5f, 0.3f, 0.8f});
+        /* ctx.circle(p6::Center{ctx.mouse()},
+                   p6::Radius{0.3f}); */
+        draw_board(3, ctx);
+    };
+    ctx.start();
 }
